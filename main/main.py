795ec9d76f1cdb714b35c777d76b0570fe920a3d
Ati Aditya <atiaditya3@gmail.com>
2019-12-01 02:36:43 +0530

    nothing

--- main/main.py
+++ main/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, requests
+from flask import Flask, render_template, request
 from forms import MachineForm, InsertService
 
 app = Flask(__name__)
@@ -6,10 +6,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f5a117a3ab54a2f5476857b652a0c8a6'
 
 @app.route('/', methods = ['GET', 'POST'])
-
+@app.route('/home', methods = ['GET', 'POST'])
 def index():
 	form = MachineForm()
-<<<<<<< HEAD
 	return render_template('home.html', form=form)
 
 @app.route('/insertservice', methods = ['GET', 'POST'])
@@ -19,15 +18,10 @@ def insert_service():
 		print(requests)
 	return render_template('insert_service.html', form=form)
 
-if(__name__ == '__main__'):
-	app.run(debug=True)
-=======
-	return render_template('index.html', form=form)
-		
 @app.route('/4options', methods = ['GET','POST'])
 def options():
 	return render_template('options.html')
 
 if(__name__ == '__main__'):
-	app.run()
->>>>>>> 41dd210aa50aaed3a1c5d1dc2c83d94cb522448c
+	app.run(debug=True)
+		
