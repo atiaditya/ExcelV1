24d8397e9776761a845cc78f17bf6727541031f0
Ati Aditya <atiaditya3@gmail.com>
2019-12-01 01:59:01 +0530

    added insert

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import InputRequired, Length
 
 class MachineForm(FlaskForm):
 	machine_serial_number= StringField('Machine Serial no', 
-		validators = [DataRequired(), Length(min=10,max=20)])
+		validators = [InputRequired(), Length(min=10,max=20)])
+	submit = SubmitField('Submit')
+
+class InsertService(FlaskForm):
+	model = StringField('Model', validators = [InputRequired()])
+	engineer_name = StringField('Engineer', validators = [InputRequired()])
+	prev_mtr_rdng = StringField('Previous Meter Reading', validators = [InputRequired()])
+	present_mtr_rdng =  StringField('Present Meter Reading', validators = [InputRequired()])
 	submit = SubmitField('Submit')
--- main/main.py
+++ main/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from forms import MachineForm
+from flask import Flask, render_template, requests
+from forms import MachineForm, InsertService
 
 app = Flask(__name__)
 
@@ -10,5 +10,13 @@ app.config['SECRET_KEY'] = 'f5a117a3ab54a2f5476857b652a0c8a6'
 def home():
 	form = MachineForm()
 	return render_template('home.html', form=form)
+
+@app.route('/insertservice', methods = ['GET', 'POST'])
+def insert_service():
+	form = InsertService()
+	if(form.validate_on_submit()):
+		print(requests)
+	return render_template('insert_service.html', form=form)
+
 if(__name__ == '__main__'):
-	app.run()
+	app.run(debug=True)
